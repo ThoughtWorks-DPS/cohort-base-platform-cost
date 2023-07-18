860213df951b974c437de55b7588c4e965e729e5
Sean Alvarez <alvie0818@gmail.com>
2023-07-18 12:01:13 -0400

    Pre-create workspaces in local mode

---
+++ tf_workspaces/variables.tf
@@ -0,0 +1,16 @@
+variable "workspace_prefix" {
+  type        = string
+  default     = "cohort-base-platform-cost"
+  description = "Prefix to use for all workspace names"
+}
+
+variable "workspace_environments" {
+  type        = list(string)
+  description = "List of environment workspaces to be created"
+}
+
+variable "tfe_token" {
+  type        = string
+  sensitive   = true
+  description = "API token used by TFE to create workspaces"
+}
